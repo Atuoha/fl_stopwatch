e18c6131ad0b5e10799897f142470e17fefc2066
Atuoha <atuohainitiatives@gmail.com>
2022-12-22 07:04:40 +0100

    impl restart and stop btn designs

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'constants/color.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,36 +9,95 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  void restart() {
+    print('restart');
+  }
+
+  void stop() {
+    print('stop');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Image.asset(
-          'assets/images/stopwatch.gif',
-          width: 120,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/stopwatch.gif',
+              width: 120,
+            ),
+            const SizedBox(height: 10),
+            const Text(
+              'Fl Stopwatch',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 26,
+                color: accentColor,
+              ),
+            ),
+            const SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                timerBox(time: '00', title: 'Hours'),
+                timerBox(time: '00', title: 'Minutes'),
+                timerBox(time: '00', title: 'Seconds'),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                buildElevatedButton(
+                  icon: Icons.restart_alt,
+                  title: 'Restart',
+                  fnc: () => restart(),
+                ),
+                const SizedBox(width: 10),
+                buildElevatedButton(
+                  icon: Icons.stop_circle_outlined,
+                  title: 'Stop',
+                  fnc: () => stop(),
+                ),
+              ],
+            ),
+          ],
         ),
-        const SizedBox(height: 10),
-        const Text(
-          'Fl Stopwatch',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 26,
-            color: accentColor,
-          ),
-        ),
-        const SizedBox(height: 40),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          timerBox(time: '00',title: 'Hours'),
-          timerBox(time: '00',title: 'Minutes'),
-          timerBox(time: '00',title: 'Seconds'),
-
-        ]),
-      ]),
-    ));
+      ),
+    );
   }
 
-  Column timerBox({required String time, required String title}) {
+  Directionality buildElevatedButton({
+    required IconData icon,
+    required String title,
+    required Function fnc,
+  }) =>
+      Directionality(
+        textDirection: TextDirection.rtl,
+        child: ElevatedButton.icon(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: accentColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+          icon: Icon(icon),
+          onPressed: () => fnc(),
+          label: Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      );
+
+  Column timerBox({
+    required String time,
+    required String title,
+  }) {
     return Column(
       children: [
         SizedBox(
